cc0776e25d5e507e8b579aa33889f49da3970f6a
teledi_makhafola <lkgahliso03@gmail.com>
2023-05-19 16:15:32 +0200

    first initial commit

---
+++ NavigationProject/ThirdVC.swift
@@ -0,0 +1,29 @@
+//
+//  ThirdVC.swift
+//  NavigationProject
+//
+//  Created by DA MAC M1 120 on 2023/05/19.
+//
+
+import UIKit
+
+class ThirdVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
